a3781899f90a7dac83b8260c61dee2fab5f1580b
mdshahbajalam7 <98772205+mdshahbajalam7@users.noreply.github.com>
2023-01-07 20:27:14 +0530

    update

--- my_projects/src/components/Linkdin/TopSection.jsx
+++ my_projects/src/components/Linkdin/TopSection.jsx
@@ -1,16 +1,109 @@
 import React from "react";
-import SwipeableTextMobileStepper from "../mui/SwipeableTextMobileStepper";
+import { Box, IconButton, useBreakpointValue } from "@chakra-ui/react";
+// Here we have used react-icons package for the icons
+import { BiChevronLeft, BiChevronRight, BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
+// And react-slick as our Carousel Lib
+import Slider from "react-slick";
+// import SwipeableTextMobileStepper from "../mui/SwipeableTextMobileStepper";
 import "./top.css";
+import Carousels from "../mui/Carousels";
 
+const settings = {
+  dots: true,
+  arrows: false,
+  fade: true,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 5000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 function TopSection() {
+  const [slider, setSlider] = React.useState(null);
+
+  const top = useBreakpointValue({ base: "90%", md: "50%" });
+  const side = useBreakpointValue({ base: "30%", md: "10px" });
+
+  // These are the images used in the slide
+  const cards = [
+    "https://www.youtube.com/embed/WjklvHqRVbs",
+    "https://www.youtube.com/embed/tSlit0vgB20",
+    "https://www.youtube.com/embed/n0xIOjbNN0A",
+  ];
   return (
     <div className="topSection">
-      <div className="topyoutube"></div>
+      <div className="topyoutube">
+        <Box
+          position={"relative"}
+          height={"400"}
+          width={"full"}
+          overflow={"hidden"}
+        >
+          {/* CSS files for react-slick */}
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charSet="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+          />
+          {/* Left Icon */}
+          <IconButton
+            aria-label="left-arrow"
+            colorScheme="messenger"
+            borderRadius="full"
+            position="absolute"
+            left={side}
+            top={top}
+            transform={"translate(0%, -50%)"}
+            zIndex={2}
+            onClick={() => slider?.slickPrev()}
+          >
+            <BiChevronLeft/>
+          </IconButton>
+          {/* Right Icon */}
+          <IconButton
+            aria-label="right-arrow"
+            colorScheme="messenger"
+            borderRadius="full"
+            position="absolute"
+            right={side}
+            top={top}
+            transform={"translate(0%, -50%)"}
+            zIndex={2}
+            onClick={() => slider?.slickNext()}
+          >
+            <BiChevronRight />
+          </IconButton>
+          {/* Slider */}
+          <Slider {...settings} ref={(slider) => setSlider(slider)}>
+            {cards.map((url, index) => (
+              <Box key={index}>
+                <iframe
+               style={{borderRadius:"15px"}}
+                  width="100%"
+                  height="360"
+                  src={url}
+                  title="YouTube videoplayer"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowfullscreen
+                ></iframe>
+              </Box>
+            ))}
+          </Slider>
+        </Box>
+      </div>
       <div className="linkdintext">
-        {/* <SwipeableTextMobileStepper/> */}
+        <h1 className="h1tage">What companies are saying about our students</h1>
+        <Carousels/>
       </div>
     </div>
   );
 }
-
 export default TopSection;
--- my_projects/src/components/YoutubSection/Youtube.jsx
+++ my_projects/src/components/YoutubSection/Youtube.jsx
@@ -4,21 +4,22 @@ import "./youtube.css";
 
 function Youtube() {
   return (
-    <div className="youtube">
-      <div className="youtubetext">
-        <h1 className="h1tag">
-          Masai & N.S.D.C. Announce Strategic Partnership.
-        </h1>
-        <p className="ptag">
-          The partnership with National Skill Development Corporation has the
-          potential to create equal employment opportunity for India’s youth,
-          narrowing the gap between the demand and supply of skilled manpower
-          for the private-sector job market. This collaboration will change the
-          way skill-based education is delivered in India.
-        </p>
-      </div>
-      <div className="youtubesection">
-        <Box
+    <>
+      <div className="youtube">
+        <div className="youtubetext">
+          <h1 className="h1tag">
+            Masai & N.S.D.C. Announce Strategic Partnership.
+          </h1>
+          <p className="ptag">
+            The partnership with National Skill Development Corporation has the
+            potential to create equal employment opportunity for India’s youth,
+            narrowing the gap between the demand and supply of skilled manpower
+            for the private-sector job market. This collaboration will change
+            the way skill-based education is delivered in India.
+          </p>
+        </div>
+        <div className="youtubesection">
+          {/* <Box
           as="iframe"
           alt="Youtube"
           src="https://youtu.be/6vOmBOvIyPE"
@@ -26,9 +27,23 @@ function Youtube() {
           sx={{
             aspectRatio: "16/10.7",
           }}
-        />
+        /> */}
+          <Box borderRadius={"10px"}>
+            <iframe
+              width="560"
+              height="315"
+              style={{ borderRadius: "10px" }}
+              src="https://www.youtube.com/embed/6vOmBOvIyPE"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </Box>
+        </div>
       </div>
-    </div>
+      <button className="btn8">START LEARNING</button>
+    </>
   );
 }
 
